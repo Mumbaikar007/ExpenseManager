2b577fd9d719b162603d193cb0124f5d1d958bad
Mumbaikar007 <brainteaser97@gmail.com>
2017-12-26 21:44:50 +0530

    "Well Played !"

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -30,38 +30,127 @@ onChanged(text){
 
 
 import React, { Component } from 'react';
-import {Card, CardSection, Input} from "./common";
-import {TextInput} from "react-native";
+import {Card, CardSection, Input, Button} from "./common";
+import { connect } from 'react-redux';
+import { expenseUpdate, expenseCreate } from '../actions';
+import { View, Text} from "react-native";
+import ModalSelector from 'react-native-modal-selector';
 
 class NewExpense extends Component {
 
+
+    constructor() {
+        super();
+
+        this.state = {
+            textInputValue: ''
+        }
+    }
+
     render(){
+
+        let index = 0;
+        const data_weekday = [
+            { key: index++, section: true, label: 'Weekday' },
+            { key: index++, label: 'Monday' },
+            { key: index++, label: 'Tuesday' },
+            { key: index++, label: 'Wednesday' },
+            { key: index++, label: 'Thursday' },
+            { key: index++, label: 'Friday' },
+            { key: index++, label: 'Saturday' },
+            { key: index++, label: 'Sunday' },
+
+        ];
+
+        const data_day = [ { key: 0, section: true, label: 'Select Day'}];
+        for ( var i = 1; i < 32; i ++){
+            data_day.push({
+                key: i,
+                label: `${i}`
+            });
+        }
 
         return (
 
             <Card>
 
                 <CardSection>
+
                     <Input
-                        label = "Expense label"
+
+                        label = "Expense"
                         placeholder = "Grocery store"
+
+                        onChangeText={value => this.props.expenseUpdate({
+                            prop: 'expense_name',
+                            value })}
+                        value = { this.props.expense_name}
                     />
+
                 </CardSection>
 
                 <CardSection>
+
                     <Input
                         label = "Amount"
                         placeholder = "Max: 10000"
+                        onChangeText={value => this.props.expenseUpdate({
+                            prop: 'amount',
+                            value })}
+                        value={this.props.amount}
                     />
+
                 </CardSection>
 
-                <CardSection></CardSection>
+                <CardSection>
+
+                    <View style = { styles.viewStyle }>
+
+                        <Text style = {styles.labelStyle}  >
+                            When
+                        </Text>
+
+                        <View style = { styles.viewStyle }>
+
+                            <ModalSelector
+
+                                style={ { marginLeft: -55}}
+
+                                data={data_day}
+                                initValue="Date"
+
+                                onChange={value => this.props.expenseUpdate({
+                                    prop: 'date',
+                                    value
+                                })}
+                            />
+
+                            <ModalSelector
+
+                                style={ {marginLeft: 15} }
+
+                                data={data_weekday}
+                                initValue="Weekday"
+
+                                onChange={value => this.props.expenseUpdate({
+                                    prop: 'day',
+                                    value
+                                })}
+                            />
+
+                        </View>
+
+                    </View>
+
+                </CardSection>
 
 
                 <CardSection>
+
                     <Button>
                         Save
                     </Button>
+
                 </CardSection>
 
             </Card>
@@ -72,5 +161,29 @@ class NewExpense extends Component {
 
 }
 
-export default NewExpense;
+const mapStateToProps = ( { expense } ) => {
+
+    const { expense_name, amount, date, day} = expense;
+
+    return { expense_name, amount, date, day };
+
+};
+
+const styles = {
+
+    viewStyle: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1,
+        marginTop: 7
+    },
+
+};
+
+export default connect ( mapStateToProps, { expenseUpdate, expenseCreate } ) (NewExpense );
 
